8b9aaefec0244dacead9c189544fc54be1c8741e
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-05-18 18:18:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//RDA Lab 1
+var studentName="Mark"; //A variable named studentName and assign it a string value.
+var studentAge="23"; //A variable named studentAge and assign it a numeric value.
+var isEnrolled="True"//A variable named isEnrolled and assign it a boolean value.
+console.log(studentName); 
+console.log(studentAge); 
+console.log(isEnrolled); 
+
+let coursename='113RDA'// String: A text value.
+let semester=1.0 // Number: An integer or floating-point number.
+let passed=true // Boolean: true or false.
+let labs=["basic js","advanced js","js webpage","js web app"] // Array: A collection of values
+const address=new Object; // Object: An entity with properties
+address .city="Toronto";
+address .province="ON";
+address .postalcode="m2g1a1";
+
+console.log(coursename);
+console.log(semester);
+console.log(passed);
+console.log(labs);
+console.log(address.city);
+
+// Arithmetic Operators:
+console.log(7+5);//addition
+// Comparison Operators:
+let x=3;
+let y=17;
+console.log(x>y);//greater than
+// Logical Operators:
+let m=true;
+let n=false;
+console.log(m||n);//logical OR
